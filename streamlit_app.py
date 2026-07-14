4fc0d5d1c823d4d91a42f8fcd2863dbfa3e650eb
Varun Illa <varunilla15@gmail.com>
2026-07-14 11:57:31 +0530

    Force TURN over TCP, remove unused packages.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -290,12 +290,7 @@ with tab_live:
                 "iceServers": [
                     {"urls": ["stun:stun.l.google.com:19302"]},
                     {
-                        "urls": ["turn:openrelay.metered.ca:80"],
-                        "username": "openrelayproject",
-                        "credential": "openrelayproject",
-                    },
-                    {
-                        "urls": ["turn:openrelay.metered.ca:443"],
+                        "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
                         "username": "openrelayproject",
                         "credential": "openrelayproject",
                     },
